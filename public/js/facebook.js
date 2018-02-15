2c1fec9e41b6385486e2204cdbd9b94fd6d56e1b
Macy Hasson <mmhasson@ucsd.edu>
2018-02-15 10:34:45 -0800

    lab 6

---
+++ public/js/facebook.js
@@ -0,0 +1,19 @@
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+        console.log('Successfully logged in with Facebook');
+         FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+  }
+
+  function changeUser(response) {
+  	facebookLogin.hide(p)
+  	#name.text(response.name)
+  	#photo.attr("src","response.picture.data.url")
+  }
